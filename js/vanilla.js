638b77eb3d9d0c20d1435f986e3b79f8b093106a
Garve Hays <gkhays@gmail.com>
2019-10-02 22:14:52 -0500

    Add basic html and js modules.

--- js/vanilla.js
+++ js/vanilla.js
@@ -3,18 +3,26 @@ function gregToJuli(n) {
   var newDate = new Date(millis);
   console.log("Input in ms: " + millis + "\n");
   newYear = newDate.getFullYear();
-  
+
   if (n == 0) console.warn("Use a non-zero input value");
-  if (newYear > 9999) console.warn("Easy there!");  
-  
+  if (newYear > 9999) console.warn("Easy there!");
+
   newDate.setFullYear(newYear - 369);             // Subtract number of years from 1601 to 1970.
-  timeStamp = newDate.getTime() + 1000*60*60*24;  // Adjust by 1 day.
+  timeStamp = newDate.getTime() + 1000 * 60 * 60 * 24;  // Adjust by 1 day.
   return timeStamp;
 }
 
 function ldapToUT(n) {
-  return new Date(n/1e4 + new Date(Date.UTC(1601,0,1)).getTime());
+  return new Date(n / 1e4 + new Date(Date.UTC(1601, 0, 1)).getTime());
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
+  $('#convert').click(function () {
+    console.log("Clicking convert button...");
+    $('#convertOld').val(gregToJuli($('#timestamp').val()));
+    var convertDate = ldapToUT($('#timestamp').val());
+    console.log("Converted date: " + convertDate);
+    $('#log').append("Human readable: " + convertDate + "\n");
+    $('#converted').val(convertDate.getTime());
+  });
 });
